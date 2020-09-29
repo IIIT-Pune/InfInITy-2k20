696e6bc47e0db931e0e76cb7e93c20a6a6cde311
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2020-09-29 13:11:10 +0530

    Navigation Bar -icons, responsive

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,76 +1,49 @@
-import React, { Component } from "react";
+import React from "react";
 import "../assets/scss/navbar.scss";
 import { NavLink } from "react-router-dom";
 
-class Navbar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <nav>
-        <ul className="navbar">
-          <li>
-            <NavLink className="--item" to="/">
-              <img
-                id="home"
-                className="--crystal"
-                alt=""
-                src={require("../assets/img/crystal_1.png")}
-              />
-              <span>Home</span>
-            </NavLink>
-          </li>
-
-          <li id="reg">
-            <NavLink className="--item" to="/registration">
-              <img
-                id="reg"
-                className="--crystal"
-                alt=""
-                src={require("../assets/img/crystal_5.png")}
-              />
-              <span>Registration</span>
-            </NavLink>
-          </li>
-
-          <li id="lstats">
-            <NavLink className="--item" to="/livestats">
-              <img
-                className="--crystal"
-                alt=""
-                src={require("../assets/img/crystal_2.png")}
-              />
-              <span> Live Stats</span>
-            </NavLink>
-          </li>
-
-          <li>
-            <NavLink className="--item" to="/about">
-              <img
-                className="--crystal"
-                alt=""
-                src={require("../assets/img/crystal_3.png")}
-              />
-              <span>About</span>
-            </NavLink>
-          </li>
-
-          <li id="hof">
-            <NavLink className="--item" to="/halloffame">
-              <img
-                className="--crystal"
-                alt=""
-                src={require("../assets/img/crystal_4.png")}
-              />
-              <span>Hall Of Fame</span>
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
+function Navbar() {
+  return (
+    <nav>
+      <ul className="navbar">
+        <li className="logo-con">
+          <NavLink exact className="--item logo" to="/">
+            <img src={require("../assets/img/asset_2.png")} alt="" />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink exact className="--item" to="/">
+            <img src={require("../assets/img/asset_1.png")} alt="" />
+            <div>Home</div>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="--item" to="/registration">
+            <img src={require("../assets/img/asset_4.png")} alt="" />
+            <div>Registration</div>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="--item" to="/livestats">
+            <img src={require("../assets/img/asset_3.png")} alt="" />
+            <div>Live Stats</div>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="--item" to="/previously">
+            <img src={require("../assets/img/asset_6.png")} alt="" />
+            <div>Previously</div>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className="--item" to="/halloffame">
+            <img src={require("../assets/img/asset_5.png")} alt="" />
+            <div>Hall of Fame</div>
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
