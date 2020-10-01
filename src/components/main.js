f5611b48c06676909c7cfc473112c85adbc3dcb6
Mukhrit <mukhritgupta20@gmail.com>
2020-10-02 01:19:31 +0530

    test

--- src/components/main.js
+++ src/components/main.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import Example from './feedbackModal';
 import LiveStats from './LiveStatsComponent';
+import HallofFame from './halloffame';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 class Main extends Component {
   render() {
     return (
       <div>
         <Switch location={this.props.location}>
-          <Route exact path="/livestats" component={() => <LiveStats />} />
-          <Route exact path="/example" component={() => <Example />} />
+          <Route exact path='/halloffame' component={() => <HallofFame />} />
+          <Route exact path='/livestats' component={() => <LiveStats />} />
+          <Route exact path='/example' component={() => <Example />} />
         </Switch>
       </div>
     );
