4f91243c474494270603450a1803cca6f36ed0b7
Jayesh Bhole <54071350+jayeshbhole@users.noreply.github.com>
2020-10-01 19:22:34 +0530

    Registration + About

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -8,7 +8,7 @@ class Home extends Component {
   render() {
     return (
       <div className="home cont">
-        <section className=" landing">
+        <section className="landing">
           <img
             className="--typography"
             src={require("../assets/img/Typography.png")}
@@ -36,7 +36,24 @@ class Home extends Component {
             alt=""
           />
         </section>
-        <section className=" about">About</section>
+        <section className="about">
+          <h2 id="abt_h2">About</h2>
+          <div className="content">
+            <p>
+              InfInITy is IIIT Pune's annual flagship coding contest hosted
+              every year on Codechef. A 3 hour coding contest that's guaranteed
+              to get your head itching and your heart wanting more. InfInITy
+              started in 2017 and was limited only to IIIT Pune.
+            </p>
+            <p>
+              The contest follows ACM style scoring and consists of problems
+              designed for particpants ranging from beginners to experts. Anyone
+              having a knack for coding is encouraged to participate.
+            </p>
+            <br />
+            <h3>Team</h3>
+          </div>
+        </section>
       </div>
     );
   }
--- src/components/main.js
+++ src/components/main.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Example from "./feedbackModal";
 import LiveStats from "./LiveStatsComponent";
 import Home from "./home";
+import Registration from "./registration";
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 class Main extends Component {
   render() {
@@ -10,6 +11,11 @@ class Main extends Component {
         <Switch location={this.props.location}>
           <Route exact path="/livestats" component={() => <LiveStats />} />
           <Route exact path="/example" component={() => <Example />} />
+          <Route
+            exact
+            path="/registration"
+            component={() => <Registration />}
+          />
           <Route exact path="/" component={() => <Home />} />
         </Switch>
       </>
